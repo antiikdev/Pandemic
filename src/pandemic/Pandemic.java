e5e1df3d3b8b38a5161719c06630017392d85f52
antiikdev <ilkka.a.kotilainen@gmail.com>
2021-12-25 12:05:45 +0800

    first

---
+++ src/pandemic/Pandemic.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package pandemic;
+
+/**
+ * Simplified simulation of virus spreading
+ * @author AntiikDev
+ * @version 25 Dec 2021
+ *
+ */
+public class Pandemic {
+
+	/**
+	 * @param args not in use
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
