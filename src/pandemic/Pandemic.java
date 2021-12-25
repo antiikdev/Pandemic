f672994355fdc23fabc2f4f5834887bd2bed6c53
antiikdev <ilkka.a.kotilainen@gmail.com>
2021-12-25 14:53:33 +0800

    2nd version

--- src/pandemic/Pandemic.java
+++ src/pandemic/Pandemic.java
@@ -14,9 +14,11 @@ public class Pandemic {
 	
 	// Matrix of "people" where 0 is no infection and 1 infected
 	static int[][] people = {
-			{ 1, 0, 0 },
-			{ 1, 0, 0 },
-			{ 1, 0, 0 } 
+			{ 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 			};
 
 	
@@ -29,34 +31,41 @@ public class Pandemic {
 	
 	
 	/**
-	 * Spreads the virus among people
+	 * Create infections by spreading the virus among people
 	 * @return 
 	 */
 	public static void spreadVirus() {
 		double infection = 0;
 		double infectionRate = 0.5; // probability of infection
 		
-		// TODO: infection possible sideways
+		// Create infections
 		for (int r = 0; r < people.length; r++) {
 			for (int c = 1; c <people[0].length; c++) {
+				// Horizontal infection spreading in the matrix with previous person 
 				if (people[r][c-1] == 1) {
 					infection = createRandomInfection();
-					System.out.println(infection);
+					// System.out.println(infection); // print random infection rate
 					if ( infection < infectionRate ) people[r][c] = 1;
 					}
+				// Sideways infection spreading with in the matrix
+				if (r != 0 && people[r-1][c-1] == 1) {
+					infection = createRandomInfection();
+					// System.out.println(infection); // print random infection rate
+					if ( infection < infectionRate ) people[r][c] = 1;
+				}
 			}
 		}
 	}
 	
 	
 	/**
-	 * Prints a two-dimensional array numbers
+	 * Prints two-dimensional array's numbers
 	 */
 	public static void printVirusSpreading() {
 		for (int r = 0; r < people.length; r++) {
 			if (r > 0) System.out.println();
 			for (int c = 0; c < people[0].length; c++) {
-				System.out.print(people[r][c]);
+				System.out.print(people[r][c] + " ");
 			}
 		}
 	}
